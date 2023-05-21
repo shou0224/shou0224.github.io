1d66af58e6d9bf2d6dd4aa07e3aabfe56df8cfdd
shou0224 <sugawarashou2@gmail.com>
2023-05-21 17:00:50 +0900

    homeicon start

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import NavBar from './NavBar'
+import HomeIcon from './HomeIcon'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <NavBar />
+      <HomeIcon />
     </div>
   )
 }
