e390988afe810bdb63d36329a988ddc7f407258c
shou0224 <sugawarashou2@gmail.com>
2023-05-21 17:15:37 +0900

    homicon ui

--- src/components/HomeIcon.jsx
+++ src/components/HomeIcon.jsx
@@ -1,8 +1,18 @@
+import { Avatar, Typography } from '@mui/material'
+import { Box, textAlign } from '@mui/system'
 import React from 'react'
 
 const HomeIcon = () => {
   return (
-    <div>HomeIcon</div>
+    <Box sx={{
+      width: '100px', 
+      height: '100px', 
+      position: 'absolute', 
+      top: 'calc(50% - 50px)', 
+      left: 'calc(50% - 50px)'}}>
+      <Avatar sx={{margin: '10px auto'}}/>
+      <Typography variant='h3'>Hello</Typography>
+    </Box>
   )
 }
 
