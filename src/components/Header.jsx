98582aae40296190e6c6cbcae4f382d8c89fe561
shou0224 <sugawarashou2@gmail.com>
2023-05-21 17:38:46 +0900

    navbar open

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import { Box, Button, Typography } from '@mui/material'
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import React from 'react'
 
-const Header = () => {
+const Header = ({onClick}) => {
   return (
     <Box sx={{width: '100%', height: '100px', backgroundColor: 'blue'}}>
       <Typography variant='h1' sx={{float: 'left'}}>Portfolio</Typography>
-      <Button sx={{float: 'right'}}>
+      <Button sx={{float: 'right'}} onClick={onClick}>
        <KeyboardBackspaceIcon sx={{fontSize: '80px', color: 'black'}} />
       </Button>
     </Box>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import NavBar from './NavBar'
 import HomeIcon from './HomeIcon'
 
 const Home = () => {
+  let [isOpen, setIsOpen] = useState(false)
+  const [open, setOpen] = useState("")
+  const toggle = () => {
+    if(!isOpen) {
+      setOpen(<NavBar />)
+    } else {
+      setOpen("")
+    }
+    setIsOpen(isOpen = !isOpen)
+  }
   return (
     <div>
-      <Header />
-      <NavBar />
+      <Header  onClick={toggle}/>
+      <div>{open}</div>
       <HomeIcon />
     </div>
   )
