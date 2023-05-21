acb1ef13dd942cdf11b5f4d67553b5c2b7353dac
shou0224 <sugawarashou2@gmail.com>
2023-05-21 16:58:29 +0900

    navbar ui

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,41 @@
+import { Avatar, List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
+import { Box } from '@mui/system'
+import HomeIcon from '@mui/icons-material/Home';
+import Person2Icon from '@mui/icons-material/Person2';
+import ArticleIcon from '@mui/icons-material/Article';
 import React from 'react'
 
 const NavBar = () => {
+  const menuList = [
+    {
+      id:1,
+      icon: <HomeIcon />,
+      name: "Home",
+    },
+    {
+      id:2,
+      icon: <Person2Icon />,
+      name: "Profile",
+    },
+    {
+      id:3,
+      icon: <ArticleIcon />,
+      name: "Portfolio",
+    }
+  ]
   return (
-    <div>NavBar</div>
+    <Box sx={{width: '150px', height: '500px', backgroundColor: 'red', float: 'right'}}>
+      <Avatar />
+      <List>
+          {menuList.map(item => (
+            <ListItem key={item.id}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText>{item.name}</ListItemText>
+            </ListItem>
+          ))}
+      </List>
+
+    </Box>
   )
 }
 
