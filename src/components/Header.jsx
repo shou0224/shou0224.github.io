3885b293b5d349032f7078778c06e6d0bd7648ab
shou0224 <sugawarashou2@gmail.com>
2023-05-21 16:40:48 +0900

    header ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
+import { Box, Button, Typography } from '@mui/material'
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Box sx={{width: '100%', height: '100px', backgroundColor: 'blue'}}>
+      <Typography variant='h1' sx={{float: 'left'}}>Portfolio</Typography>
+      <Button sx={{float: 'right'}}>
+       <KeyboardBackspaceIcon sx={{fontSize: '80px', color: 'black'}} />
+      </Button>
+    </Box>
   )
 }
 
