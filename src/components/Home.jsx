a027d630683a3162bb9daf26e9f8ca75c5afc8aa
shou0224 <sugawarashou2@gmail.com>
2023-05-21 16:45:51 +0900

    navbar start

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import NavBar from './NavBar'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <NavBar />
     </div>
   )
 }
