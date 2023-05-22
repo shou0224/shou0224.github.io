81258d838431d4c75857d485de72b3547118e80e
shou0224 <sugawarashou2@gmail.com>
2023-05-22 16:46:25 +0900

    contact design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,17 @@
+import { Button } from '@mui/base'
+import { TextField, Typography } from '@mui/material'
+import { Box, margin } from '@mui/system'
 import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <Box component='form' sx={{width: "300px", margin: '10px auto'}}>
+      <Typography>Contact</Typography>
+      <TextField fullWidth={true} sx={{margin:'10px'}} id="standard-basic" label="お名前" variant="standard"/>
+      <TextField fullWidth={true} sx={{margin:'10px'}} id="standard-basic" label="メールアドレス" variant="standard"/>
+      <TextField fullWidth={true} sx={{margin:'10px'}} id="outlined-basic" label="ご内容" variant="outlined" />
+      <Button>送信</Button>
+    </Box>
   )
 }
 
