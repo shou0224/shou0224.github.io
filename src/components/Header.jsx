d122152c6eb88cde6421f357f0b2c3d7ab9e9d0f
shou0224 <sugawarashou2@gmail.com>
2023-05-22 14:35:15 +0900

    header arrow adjust

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
 import { Box, Button, Typography } from '@mui/material'
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const Header = () => {
   let [isOpen, setIsOpen] = useState(false)
+  const [arrows, setArrows] = useState(<KeyboardBackspaceIcon sx={{fontSize: '80px', color: 'black'}} />)
   const [open, setOpen] = useState("")
   const toggle = () => {
     if(!isOpen) {
       setOpen(<NavBar />)
+      setArrows(<ArrowRightAltIcon sx={{fontSize: '80px', color: 'black'}}/>)
     } else {
       setOpen("")
+      setArrows(<KeyboardBackspaceIcon sx={{fontSize: '80px', color: 'black'}} />)
     }
     setIsOpen(isOpen = !isOpen)
   }
@@ -19,7 +23,8 @@ const Header = () => {
     <Box sx={{width: '100%', height: '100px', backgroundColor: 'blue'}}>
       <Typography variant='h1' sx={{float: 'left'}}>Portfolio</Typography>
       <Button sx={{float: 'right'}} onClick={toggle}>
-       <KeyboardBackspaceIcon sx={{fontSize: '80px', color: 'black'}} />
+        <div>{arrows}</div>
+       {/* <KeyboardBackspaceIcon sx={{fontSize: '80px', color: 'black'}} /> */}
       </Button>
     </Box>
     <div>{open}</div>
